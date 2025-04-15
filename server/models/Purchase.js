3abf30aafb58cf229de67b34dc3453ce2b9e6026
janarthananKV <jkm06032005@gmail.com>
2025-04-15 10:10:32 +0530

    model corrected

--- server/models/Purchase.js
+++ server/models/Purchase.js
@@ -4,7 +4,7 @@ const purchaseSchema = new mongoose.Schema({
     courseId : {type : mongoose.Schema.Types.ObjectId, ref : 'Course', required : true},
     userId : {type : String, ref : 'User', required : true},
     amount : {type : Number, required : true},
-    status : {type : String, enum : ['pending', 'completed', 'failed'], deafult : 'pending'}
+    status : {type : String, enum : ['pending', 'completed', 'failed'], default : 'pending'}
 }, {timestamps : true})
 
 const Purchase = mongoose.model('Purchase', purchaseSchema)
