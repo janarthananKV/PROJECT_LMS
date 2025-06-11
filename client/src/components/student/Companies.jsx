dd354cb0bbb587edda5c2907eec4c8d76e1355f4
janarthananKV <jkm06032005@gmail.com>
2025-06-11 13:05:46 +0530

    Added logo slider

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
+import React, { useEffect, useRef } from 'react';
+import { assets } from '../../assets/assets';
 
 const Companies = () => {
+  const scrollContainerRef = useRef(null);
+
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+    const ul = container.querySelector('ul');
+
+    // Clone the original list to create a seamless loop
+    const cloned = ul.cloneNode(true);
+    cloned.setAttribute('aria-hidden', 'true');
+    container.appendChild(cloned);
+  }, []);
+
   return (
     <div className='pt-16'>
-      <p className='text-base text-gray-500'>Trusted by learners from</p>
-      <div className='flex flex-wrap items-center justify-center gap-6 md:gap-16
-      md:mt-10 mt-5'>
-        <img src={assets.microsoft_logo} alt="microsoft" className='w-20 md:w-28' />
-        <img src={assets.walmart_logo} alt="walmart" className='w-20 md:w-28' />
-        <img src={assets.accenture_logo} alt="accenture" className='w-20 md:w-28' />
-        <img src={assets.adobe_logo} alt="adobe" className='w-20 md:w-28' />
-        <img src={assets.paypal_logo} alt="paypal" className='w-20 md:w-28' />
+      <p className='text-base text-gray-500 text-center mb-4'>
+        Trusted by learners from
+      </p>
 
+      {/* Gradient mask wrapper with 65% width and center alignment */}
+      <div className="w-[65%] mx-auto overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_80px,_black_calc(100%-80px),transparent_100%)]">
+        {/* Scrolling content */}
+        <div
+          ref={scrollContainerRef}
+          className='inline-flex flex-nowrap'
+        >
+          <ul className="flex items-center [&_li]:mx-6 md:[&_li]:mx-12 [&_img]:max-w-none animate-loop-scroll">
+            <li><img src={assets.microsoft_logo} alt="Microsoft" className='w-20 md:w-28' /></li>
+            <li><img src={assets.walmart_logo} alt="Walmart" className='w-20 md:w-28' /></li>
+            <li><img src={assets.accenture_logo} alt="Accenture" className='w-20 md:w-28' /></li>
+            <li><img src={assets.adobe_logo} alt="Adobe" className='w-20 md:w-28' /></li>
+            <li><img src={assets.paypal_logo} alt="PayPal" className='w-20 md:w-28' /></li>
+          </ul>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Companies
+export default Companies;
